43814a5664e8925103c1ca83c54ee898ad627359
Máté Stiegelmayer <stigimate@gmail.com>
2020-05-25 17:01:48 +0200

    Added result package

--- src/main/java/game/result/GameResultDao.java
+++ src/main/java/game/result/GameResultDao.java
@@ -1,5 +1,32 @@
 package game.result;
 
-public class GameResultDao {
+import com.google.inject.persist.Transactional;
+import util.jpa.GenericJpaDao;
+
+import java.util.List;
+
+/**
+ * DAO class for the {@link GameResult} entity.
+ */
+public class GameResultDao extends GenericJpaDao<GameResult> {
+
+    public GameResultDao() {
+        super(GameResult.class);
+    }
+
+    /**
+     * Returns the list of {@code n} latest results for solving the puzzle.
+     *
+     * @param n the maximum number of results to be returned
+     * @return the list of {@code n} best results with respect to the time
+     * spent for solving the puzzle
+     */
+    @Transactional
+    public List<GameResult> findBest(int n) {
+        return entityManager.createQuery("SELECT r FROM GameResult r WHERE r.solved = true ORDER BY r.created DESC, r.duration ASC", GameResult.class)
+                .setMaxResults(n)
+                .getResultList();
+    }
 
 }
+
