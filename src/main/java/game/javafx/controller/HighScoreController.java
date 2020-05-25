a2a08b521f37387dac9c064b1e970b7ee0b74d2b
Máté Stiegelmayer <stigimate@gmail.com>
2020-05-25 18:21:27 +0200

    Bugfix#4

--- src/main/java/game/javafx/controller/HighScoreController.java
+++ src/main/java/game/javafx/controller/HighScoreController.java
@@ -5,31 +5,23 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 import game.result.GameResult;
 import game.result.GameResultDao;
 
 import javax.inject.Inject;
-import java.io.IOException;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 @Slf4j
 public class HighScoreController {
